cb0969d816d2764c571b3f71fce82a771d5eabfd
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2017-12-06 22:54:35 +0530

    Commit-2 Game ready for 2 playes with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RulesComponent } from './rules/rules.component';
 import { GamePlayComponent } from './game-play/game-play.component';
 import { HeaderComponent } from './header/header.component';
 
+const appRoutes:Routes = [
+  { path: '', redirectTo: 'rules', pathMatch: 'full' },
+  { path: 'rules', component: RulesComponent},
+  { path: 'game-play', component: GamePlayComponent },
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game-play/game-play.component.ts
+++ src/app/game-play/game-play.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild,Component, OnInit, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-game-play',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePlayComponent implements OnInit {
 
+  @ViewChild('finalScoreInput') finalScoreInput: ElementRef;
+
+  playersScore: Array<number>;
+  roundScore;
+  diceScore: number
+  activePlayer: number;
+  finalScore: number;
+  showDice: boolean;
+  isGameActive: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.onGameInit();
+  }
+
+  onSetScore() {
+    this.finalScore = this.finalScoreInput.nativeElement.value;
+    this.isGameActive = true;
+    this.showDice = true;
+  }
+
+  onGameInit() {
+    this.playersScore = [0, 0];
+    this.roundScore = { player0: 0, player1: 0 };
+    this.activePlayer = 0;
+    this.finalScore = 0;
+    this.showDice = false;
+    this.isGameActive = false;
+    this.diceScore = 5;
+    this.finalScoreInput.nativeElement.focus();
+  }
+
+  onDiceRoll() {
+    this.diceScore = Math.floor(Math.random() * 6) + 1;
+    if (this.diceScore === 1) {
+      this.roundScore[`player${this.activePlayer}`] = 0;
+      this.activePlayer = 1 - this.activePlayer;  // Toggle between 0 and 1
+    } else {
+      this.roundScore[`player${this.activePlayer}`] += this.diceScore;
+    }
+  }
+
+  onScoreHold() {
+    let { roundScore, activePlayer } = this;
+    this.playersScore[activePlayer] = roundScore[`player${this.activePlayer}`];
+    this.roundScore[`player${this.activePlayer}`] = 0;
+    this.activePlayer = 1 - activePlayer;  // Toggle between 0 and 1
   }
 
 }
